c9ae691a28d880182b31f109a69115a8b9947d5a
landolm2M <landolm2@MILWCL01072>
2019-11-24 11:04:04 +0100

    visualizzati risultati ricerca

--- app/Presenters/ListingPresenter.php
+++ app/Presenters/ListingPresenter.php
@@ -8,9 +8,28 @@ use Nette;
 
 final class ListingPresenter extends _BasePresenter
 {
-    public function renderSearch()
+    public function renderSearchResults($page = 1, $limit = 10)
     {
-        $this->template->searchResults = $this->dbWrapper->searchPosts();
+        $results = [
+            'posts' => [],
+            'page' => [],
+            'pageNo' => 1,
+            'pageTot' => 1,
+            'limit' => $limit
+        ];
+
+        $results['posts'] = $this->dbWrapper->searchPosts();
+        $results['pageTot'] = \count($results['posts']) / $limit;
+
+        $rowFrom = ($page - 1) * $limit;
+        $rowTo = $page * $limit;
+        for ($i = $rowFrom; $i < $rowTo; $i++) {
+            if (!empty($results['posts'][$i])) {
+                $results['page'][] = $results['posts'][$i];
+            }
+        }
+
+        $this->template->searchResults = $results;
     }
 
     public function renderDetail($postId)
