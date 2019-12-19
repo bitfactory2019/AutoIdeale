7e55b5c904e5a0745246e5ef4d1ff026598911cd
bitfactory2019 <bitfactory2019@gmail.com>
2019-12-19 10:29:49 +0100

    aggiunto clone

---
+++ www/clone.php
@@ -0,0 +1,48 @@
+<?php
+    function rmdir_recursive($dir) {
+        foreach(scandir($dir) as $file) {
+            if ('.' === $file || '..' === $file) continue;
+            if (is_dir($dir.'/'.$file)) rmdir_recursive($dir.'/'.$file);
+            else unlink($dir.'/'.$file);
+        }
+
+        rmdir($dir);
+    }
+
+    function copy_recursive($src, $dst) {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while(false !== ( $file = readdir($dir)) ) {
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if ( is_dir($src . '/' . $file) ) {
+                    copy_recursive($src . '/' . $file,$dst . '/' . $file);
+                }
+                else {
+                    copy($src . '/' . $file,$dst . '/' . $file);
+                }
+            }
+        }
+
+        closedir($dir);
+    }
+
+    file_put_contents("../../AutoIdeale-master.zip", fopen('https://github.com/bitfactory2019/AutoIdeale/archive/master.zip', 'r'));
+
+    $zip = new ZipArchive;
+    $zip->open("../../AutoIdeale-master.zip");
+
+    copy_recursive("../../AutoIdeale-master/www/images", "../../AutoIdeale-temp");
+    rmdir_recursive("../../AutoIdeale-master");
+    $zip->extractTo("../../");
+    $zip->close();
+    copy_recursive("../../AutoIdeale-temp", "../../AutoIdeale-master/www/images");
+    rmdir_recursive("../../AutoIdeale-temp");
+    ?>
+    <html>
+        <body>
+            <div style="text-align: center;color: white; background: green; font: bold 32pt monospace; padding: 40px; margin: 100px;">
+                AutoIdeale aggiornato correttamente
+            </div>
+            <p align="center"><a href="http:/www.autoideale.it">Vai al sito</a></p>
+        </body>
+    </html>
