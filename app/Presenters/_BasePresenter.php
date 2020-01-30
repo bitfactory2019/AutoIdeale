b0441fa3f1a36cb736a5717d453c6c3d4e453d18
Mario <bitfactory2019@gmail.com>
2020-01-30 07:04:02 +0100

    aggiunto facebookShare mancante

--- app/Presenters/_BasePresenter.php
+++ app/Presenters/_BasePresenter.php
@@ -36,11 +36,25 @@ abstract class _BasePresenter extends Nette\Application\UI\Presenter
         $this->section_admin = $this->getSession('admin');
         $this->section_frontend = $this->getSession('frontend');
 
+        $this->template->facebookShare = $this->_getFacebookShare();
+
         $this->template->logged = !empty($this->section_admin->user);
         $this->template->user = $this->dbWrapper->getUserById($this->section_admin->user_id);
 
         $this->template->topBrands = $this->dbWrapper->getTopBrands(6, 4);
         $this->template->userWishlist = $this->dbWrapper->getUserWishlist($this->section_admin->user_id);
+    }
+
+    protected function _getFacebookShare()
+    {
+        $facebookShare = new \StdClass();
+
+        $facebookShare->url = \str_replace(':80', '', $this->getHttpRequest()->getUrl());
+        $facebookShare->title = 'AutoIdeale.it';
+        $facebookShare->description = 'Acquista la tua auto';
+        $facebookShare->image = 'https://www.autoideale.it/img/gallery/slide1.jpg';
+
+        return $facebookShare;
     }
 
     public function getDbService(): \Nette\Database\Context
