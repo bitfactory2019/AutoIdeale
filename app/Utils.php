0e6edf43ff2a607cede74ede944f551e940d7aae
Mario <bitfactory2019@gmail.com>
2020-02-12 05:47:56 +0100

    rinominato campo tabella posts_images.post_id in posts_images.posts_id

--- app/Utils.php
+++ app/Utils.php
@@ -147,12 +147,12 @@ class Utils
 
     public function formatPostResult($post)
     {
-        $thumbnail = $this->presenter->getDbService()->table('posts_images')->where('post_id', $post->id)->limit(1)->fetch();
+        $thumbnail = $this->presenter->getDbService()->table('posts_images')->where('posts_id', $post->id)->limit(1)->fetch();
 
         return [
             "data" => $post,
             "thumbnail" => $thumbnail,
-            "images" => $post->related('posts_images.post_id'),
+            "images" => $post->related('posts_images.posts_id'),
             "isNew" => $post->creation_time > strtotime("3 days ago"),
             "isNotAvailable" => !$post->approved
         ];
